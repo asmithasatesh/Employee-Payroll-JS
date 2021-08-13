15fa06afadb84b7e9d353d4c1e0c0a64204ce910
[Asmitha] <[asmithasatesh@gmail.com]>
2021-08-13 18:46:08 +0530

    [Asmitha]Refactor: Add Function in UC 2

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -18,29 +18,33 @@ const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
 
-var empHrs=0;
-var empCheck= Math.floor(Math.random()*10) % 3;
-//Emploee Type
-var employees=new Array();
-employees.push("Absentees");
-employees.push("Part Timer");
-employees.push("Full timer");
-
-console.log("Usecase 2: Employee is "+employees[empCheck]+empCheck);
-switch(empCheck)
+//Refactor UC 2 with Function
+function CalculateDailyWage()
 {
-    case IS_PART_TIME:
-        empHrs=PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHrs=FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs=0;
-        break;
+    var empHrs=0;
+    var empCheck= Math.floor(Math.random()*10) % 3;
+    //Emploee Type
+    var employees=new Array();
+    employees.push("Absentees");
+    employees.push("Part Timer");
+    employees.push("Full timer");
+
+    console.log("Usecase 2: Employee is "+employees[empCheck]+empCheck);
+    switch(empCheck)
+    {
+        case IS_PART_TIME:
+            empHrs=PART_TIME_HOURS;
+            break;
+        case IS_FULL_TIME:
+            empHrs=FULL_TIME_HOURS;
+            break;
+        default:
+            empHrs=0;
+            break;
+    }
+    let empWage=empHrs*WAGE_PER_HOUR;
+    console.log("Emp Wage = "+empWage);
 }
-let empWage=empHrs*WAGE_PER_HOUR;
-console.log("Emp Wage = "+empWage);
 
 
 
